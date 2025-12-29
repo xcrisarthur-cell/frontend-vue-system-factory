602857e1b2dd0c06864315f2167a67fe3d4fdbe1
xartricris <xcrisarthur@gmail.com>
2025-12-29 09:02:43 +0700

    vercel

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild', // Changed from 'terser' to 'esbuild' (faster, no extra dependency)
     rollupOptions: {
       output: {
         manualChunks: {
